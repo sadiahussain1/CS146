a0d4803ac5aa19db0d65cbf182835d287c1d6dc7
sadiahussain1 <111794674+sadiahussain1@users.noreply.github.com>
2024-02-01 22:14:32 -0800

    Create HW1.py
    yay im not looking forward to trying to make this in java

---
+++ HW/HW1/HW1.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def palindrome(self, s):     #inputs: string s
+
+        day = ""        #a blank string to get rid of spaces and numbers and the like
+        for currentLetter in s: #cycle thru the letters in string
+            if (currentLetter.isalpha()):   #if its an actual letter
+                day += currentLetter        #add it to our letters-only string
+
+        day = day.lower()         #AND LOWER IT god ur so picky
+        oppositeday = day[::-1]   #tbh i just looked up "how to reverse string" and i guess this slices it up all fancy lol. then i save it as opposite day
+
+        if (oppositeday == day) : #if it still equals the og string then yayyyy woohooooo
+            return(True)
+
+        return(False)       #else, return false :(
+
+    #ughhhh the cycling thru and making sure all the letters r lowercase is such a painnnnnn
+
+
+test = Solution()
+print("tacocat being a palindrome is", test.palindrome("tacocat"))
+print("burritocat being a palindrome is", test.palindrome("burritocat"))
